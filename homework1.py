18eb16bb179bbe3dc655fcb4a10862caf2a64afa
AFRIKAKORPS1 <trevorwalker61@gmail.com>
2016-09-14 13:40:19 -0700

    second revision

--- homework1.py
+++ homework1.py
@@ -1,30 +1,42 @@
+"""
+Author: Trevor Walker
+Work: Assignment 2
+date: 9/14/2016
+"""
+
 import random
 import math
 import itertools
 
-count = 1 #count for basic loop control
+count = 1  #count for basic loop control
 pairs = []
-finalPairs = [] #lists to store random numbers and pairs
+finalPairs = []  #lists to store random numbers and pairs
 
 #while loop to create random numbers, and then form pairs stored into proper list
-while (count <= 4):
+while count <= 4:
     points1 = random.randint(-180, 180)
     points2 = random.randint(-90, 90)
     pairs = points1, points2
     finalPairs.append(pairs)
-    count = count + 1
+    count += 1
 
 print finalPairs
 print len(finalPairs)
 
+#euclidean formula for distance
 def distance(points):
     p0, p1 = points
     return math.sqrt((p0[0] - p1[0])**2 + (p0[1] - p1[1])**2)
 
+#test function to determine if distance is working
 min_pair = min(itertools.combinations(finalPairs, 2), key=distance)
 min_distance = distance(min_pair)
 print min_distance
 
-class PointCalc():
+
+#initial class declaration
+class PointCalc:
     def __init__(self):
-        pair1 = 1
+        pair1=1
+
+
