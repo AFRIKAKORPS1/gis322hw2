2cb2fa150f6cb922f0f0970e3073078b1d2ad1bc
AFRIKAKORPS1 <trevorwalker61@gmail.com>
2016-09-14 20:20:02 -0700

    fina; revision

--- homework1.py
+++ homework1.py
@@ -3,10 +3,11 @@ Author: Trevor Walker
 Work: Assignment 2
 date: 9/14/2016
 """
-
 import random
 import math
 import itertools
+import scipy
+from scipy import spatial
 
 count = 1  #count for basic loop control
 pairs = []
@@ -20,23 +21,37 @@ while count <= 4:
     finalPairs.append(pairs)
     count += 1
 
-print finalPairs
-print len(finalPairs)
-
-#euclidean formula for distance
+#euclidean formula for distance, used in min_pair to find least distance
 def distance(points):
-    p0, p1 = points
-    return math.sqrt((p0[0] - p1[0])**2 + (p0[1] - p1[1])**2)
+        p0, p1 = points
+        return math.sqrt((p0[0] - p1[0])**2 + (p0[1] - p1[1])**2)
 
-#test function to determine if distance is working
+#test function to determine minimum distance
 min_pair = min(itertools.combinations(finalPairs, 2), key=distance)
 min_distance = distance(min_pair)
-print min_distance
-
 
 #initial class declaration
 class PointCalc:
     def __init__(self):
-        pair1=1
+       print '\nX and Y coordinate pairs: '
+       print finalPairs
+
+    def allCoords(self, x):
+        print '\nThe shortest distance between coordinate pairs is: '
+        listDist = scipy.spatial.distance.pdist(finalPairs, 'euclidean') #scipy method to find calculated distance
+        print listDist
+
+        print '\nThe smallest coordinate is: '
+        print min(finalPairs)
+
+        print '\nAnd the smallest distance is: '
+        print min_distance
+
+#test
+test = PointCalc()
+test.allCoords(finalPairs)
+print test
+
+
 
 
